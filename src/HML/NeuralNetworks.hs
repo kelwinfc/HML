dac90daafc2a8234e79bfcfc54661052fccc385b
kelwinfc <kelwinfc@lisboa.(none)>
2012-07-07 22:04:35 -0430

    Finalizado Kmeans paralelo.

--- src/HML/NeuralNetworks.hs
+++ src/HML/NeuralNetworks.hs
@@ -1,10 +1,11 @@
 module HML.NeuralNetworks(Neuron, LinearUnit(LinearU), StepUnit(StepU),
                           SigmoidUnit(SigmoidU),
-                          MultiLayerPerceptron, createNeuralNetork,
-                          (~>))
+                          NeuralNetwork(ANN), createNeuralNetork,
+                          (~>), (~~>))
     where
 
 import HML.Prelude
+import Data.List
 
 class Neuron a where
     
@@ -32,9 +33,80 @@ instance Neuron SigmoidUnit where
     (~>) entries (SigmoidU weights) =
         1.0 / ( 1.0 + (exp (- (neuralDot entries weights) )))
 
-data MultiLayerPerceptron = ANN [LinearUnit] [[SigmoidUnit]]
+data NeuralNetwork = ANN [LinearUnit] [[SigmoidUnit]]
 
-createNeuralNetork :: [Int] -> MultiLayerPerceptron
+createNeuralNetork :: [Int] -> NeuralNetwork
 createNeuralNetork (x:xs) = ANN [] [[]]
 
 neuralDot a b = sum $ zipWith (*) (1:a) b
+
+outputLayer :: Neuron a => [Double] -> [a] -> [Double]
+outputLayer entries = map (entries ~>)
+
+(~~>) :: [Double] -> NeuralNetwork -> [Double]
+(~~>) a (ANN entry hidden) = foldl' outputLayer
+                                    (outputLayer a entry)
+                                    hidden
+
+outputs :: [Double] -> NeuralNetwork -> [[Double]]
+outputs a (ANN e h) = scanl outputLayer (outputLayer a e) h
+
+deltas ::  [Double]         -- Target function
+        -> [[SigmoidUnit]]  -- hidden and output Units
+        -> [[Double]]       -- Outputs
+        -> [[Double]]       -- Deltas
+
+deltas target _ [o]   = [zipWith (\s y -> s * (1 - s) * (y - s)) o target]
+deltas target (wh: w_hs@(w_next:_) ) (o_h : o_hs@(o_next:_) ) =
+        (zipWith (*) accum o_aux) : delta_hs
+    
+    where delta_hs :: [[Double]]
+          delta_hs@(d_next:_) = deltas target w_hs o_hs
+          
+          o_aux :: [Double]
+          o_aux = map (\x -> x*(1-x)) o_next
+          
+          accum :: [Double]
+          accum = foldl1 (zipWith (+)) weights_times_deltas
+          
+          weights_times_deltas :: [[Double]]
+          weights_times_deltas = zipWith (\(SigmoidU (_:w) ) d -> map (*d) w)
+                                         w_next d_next
+
+add_delta :: SigmoidUnit -> Double -> [Double] -> SigmoidUnit
+add_delta (SigmoidU weights) alpha delta = 
+    SigmoidU (zipWith (\w d -> w + alpha*d) weights delta)
+
+new_weights :: NeuralNetwork -> Double -> [[[Double]]] -> NeuralNetwork
+new_weights (ANN l []) alpha [] = ANN l []
+new_weights (ANN l (h:hs)) alpha (dl:dls) = ANN l (new_layer:aux)
+    where new_layer = zipWith go h dl
+          go n d = add_delta n alpha d
+          ANN _ aux = new_weights (ANN l hs) alpha dls
+
+{-
+backProp :: Double -> NeuralNetwork -> [([Double],Double)] -> NeuralNetwork
+backProp alpha nn@(ANN _ w) tr = nn--new_weights nn alpha d
+    where o = map (\(x,y) -> new_weights nn alpha (deltas y w (outputs x nn))) tr
+          --d = map (\y -> deltas y w o) ys
+          -}
+{- procesar la capa --> llamar a la siguiente
+                    <-- calcular el delta
+                    --- calcular los nuevos pesos
+-}
+{-
+    -- | Train the given neural network using the backpropagation algorithm on the given sample with the given learning ratio (alpha)
+    backPropU :: Double -> [[Neuron]] -> (UArr Double, UArr Double) -> [[Neuron]]
+    backPropU alpha nss (xs, ys) = [aux (head nss) ds_hidden xs
+                        ,aux (nss !! 1) ds_out output_hidden]
+    
+    where 
+      output_hidden = computeLayerU (head nss) xs
+      output_out = computeLayerU (nss !! 1) output_hidden
+      
+      ds_out = zipWithU (\s y -> s * (1 - s) * (y - s)) output_out ys
+      ds_hidden = zipWithU (\x s -> x * (1-x) * s) output_hidden . toU $ map (sumU . zipWithU (*) ds_out) . map toU . transpose . map (fromU . weights) $ (nss !! 1)
+      
+      aux ns ds xs = zipWith (\n d -> n { weights = zipWithU (\w x -> w + alpha * d * x) (weights n) xs }) ns (fromU ds)
+
+-}
